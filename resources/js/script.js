e97d35d8fe95656c94034f8873f95106682a3a72
kikimutiara <31962693+kikimutiara@users.noreply.github.com>
2018-01-04 15:37:38 +1000

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('h1').click(function() {
+        $(this).css('background-color', '#ff0000');
+    });
+    
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction === "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        };
+    });
+    
+
+});
